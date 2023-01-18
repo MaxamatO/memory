3218ddaa7327a6ad7500556043e39dde6a8d87f6
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-01-18 16:57:05 +0100

    Add checking for pairs

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,3 +1,4 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils/misc/isDisabled";
 import React, { createRef, useEffect, useRef, useState } from "react"
 import styled from "styled-components";
 
@@ -6,6 +7,7 @@ function Card() {
     const cardRef = useRef([]);
     const countClicksRef = useRef(0);
     const cardsClickedRef = useRef([]);
+    const cardsGuessed = useRef([])
 
     useEffect(() => {
         // eslint-disable-next-line
@@ -54,14 +56,14 @@ function Card() {
     function show(id){
         let divs = cardRef.current;
         for(let i=0; i<divs.length; i++){
-            if(divs[i].id===id && !cardsClickedRef.current.includes(id)){
+            if(divs[i].id===id && !cardsClickedRef.current.includes(id) && !cardsGuessed.current.includes(cards.find((card) => card.id===id))){
+                console.log(cardsGuessed.current);
                 divs[i].firstChild.className="show";
                 countClicksRef.current = countClicksRef.current+1;
                 cardsClickedRef.current.push(id);
             }
         }
         if(countClicksRef.current===2){
-            console.log("???");
             setTimeout(()=>{
                 hide(cardsClickedRef.current);
             }, 500)
@@ -72,29 +74,57 @@ function Card() {
 
     function hide(ids){
         let divs = cardRef.current;
-        console.log(ids);
-        console.log(cardRef.current);
-        for(let i=0; i<divs.length; i++){
-            if(divs[i].id===ids[0]){
-                divs[i].firstChild.className = "hidden";
-            }
-            if(divs[i].id===ids[1]){
-                divs[i].firstChild.className = "hidden";
-            }
+        let card1 = cards.find((card) => card.id===ids[0]);
+        let card2 = cards.find((card) => card.id===ids[1]);
+        if(check(card1, card2)){
+            cardsGuessed.current.push(card1.id, card2.id)
+            disable(card1.id, card2.id);
+        }  
+        else{
+            for(let i=0; i<divs.length; i++){
+                let divToWorkWith = divs[i].firstChild;
+                
+            
+                    if(divs[i].id===ids[0] && !cardsGuessed.current.includes(divs[i].id)){
+                        divToWorkWith.className = "hidden";
+                    }
+                    if(divs[i].id===ids[1] && !cardsGuessed.current.includes(divs[i].id)){
+                        divToWorkWith.className = "hidden";
+                    }
+                }
         }
         countClicksRef.current = 0;
         cardsClickedRef.current = [];
-        
+    }
 
-       
+    function check(card1, card2){
+        console.log(card1.value, card2.value);
+        if(card1.value === card2.value && !cardsGuessed.current.includes(card1.id) && !cardsGuessed.current.includes(card2.id)){
+            console.log(cardsGuessed.current);
+
+            return true;
+        }
+        return false;
+    }
+
+    function disable(id1, id2){
+        let divs = cardRef.current;
+        for(let i=0; i<divs.length; i++){
+            let divToWorkWith = divs[i];
+                if(divs[i].id===id1){
+                    divToWorkWith.classList.add("disabled");
+                }
+                if(divs[i].id===id2){
+                    divToWorkWith.classList.add("disabled");
+                }
+            }
     }
 
     return(
         <>
             {cards.map((card, i) => {
                 return(
-                    
-                        <MemoryCard key={card.id} id={card.id} ref={(ref) => (cardRef.current[i] = ref)} onClick={()=>show(card.id)}>
+                    <MemoryCard key={card.id} id={card.id} ref={(ref) => (cardRef.current[i] = ref)} onClick={()=>show(card.id)}>
                             <p className="hidden">{card.value}</p>
                         </MemoryCard>
                     
@@ -104,9 +134,12 @@ function Card() {
     )
 }
 
-const MemoryCard = styled.div`
+const MemoryCard = styled.button`
     background-color: #A99985;
     text-align: center;
+    width: 100%;
+    height: 100%;
+    border: none;
 
     :hover{
         background-color: #999999;
@@ -121,6 +154,11 @@ const MemoryCard = styled.div`
     p{
         font-size: 30px;
         margin-top: 50%;
+    }
+    .disabled{
+        
+            background-color: #70798C;
+        
     }
 `
 
