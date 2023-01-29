81351395a455db59878497494d9ab877d84ed773
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-01-29 19:52:51 +0100

    Add helper functions

---
+++ src/Redu.js
@@ -0,0 +1,38 @@
+function generateRandomNumber(){
+    return Math.floor(Math.random() * (20 - 1 + 1) + 1)
+}
+
+export default function populate(){
+    let array = [];
+    let usedNumbers = [];
+    let i = 0;
+    while(i<10){
+        let random = generateRandomNumber();
+            if(!usedNumbers.includes(random)){
+                usedNumbers.push(random)
+                array.push({"value":random, "id": crypto.randomUUID()});
+                array.push({"value":random, "id": crypto.randomUUID()});
+                i++;
+            }
+    }
+    shuffle(array);
+    return array;
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
