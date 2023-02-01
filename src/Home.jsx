1f8cdd4f450b24131295d49c7767060a0b67147b
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-02-01 22:23:07 +0100

    Add menu

---
+++ src/Home.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Pairs from "./Pairs";
+import styled from "styled-components";
+import "./index.css";
+function Home() {
+  const [settings, setSettings] = useState({
+    game: "",
+    level: "",
+  });
+
+  const renderGame = () => {
+    switch (settings.game) {
+      case "Find Pairs":
+        return <Pairs level={settings.level} />;
+      case "Reaction Time Test":
+        return <ComingSoon>Coming Soon</ComingSoon>;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <>
+      {settings.game === "" ? (
+        <MenuContainer>
+          <GameOption onClick={() => setSettings({ game: "Find Pairs" })}>
+            Find Pairs
+          </GameOption>
+          <GameOption
+            onClick={() => setSettings({ game: "Reaction Time Test" })}
+          >
+            Reaction Time Test <br />
+          </GameOption>
+        </MenuContainer>
+      ) : (
+        renderGame()
+      )}
+    </>
+  );
+}
+
+const MenuContainer = styled.div`
+  width: 100vw;
+  height: 20vh;
+  background-color: var(--board-color);
+  display: flex;
+  place-content: center;
+  align-items: center;
+  justify-content: space-evenly;
+  margin: auto auto;
+`;
+
+const GameOption = styled.a`
+  text-decoration: none;
+  font-size: 2.2em;
+  :hover {
+    cursor: pointer;
+    color: var(--bg-color);
+  }
+`;
+
+const ComingSoon = styled.h1`
+  font-size: 3rem;
+`;
+
+export default Home;
