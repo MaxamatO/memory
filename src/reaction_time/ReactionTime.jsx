535ebb66afdefead89fa856be21b6fbf15f1658d
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-02-03 00:13:30 +0100

    Add change color on click

--- src/reaction_time/ReactionTime.jsx
+++ src/reaction_time/ReactionTime.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const RED = "#d47c7c";
+const GREEN = "#9ccc74";
 
 function ReactionTime() {
-  return <>Hello</>;
+  const [backgroundClicker, setBackgroundClicker] = useState(RED);
+
+  // const timeToChangeColorRef = userRef();
+
+  const changeColor = () => {
+    backgroundClicker === RED
+      ? setBackgroundClicker(GREEN)
+      : setBackgroundClicker(RED);
+  };
+
+  return (
+    <>
+      <Container>
+        <MainContent
+          $start={backgroundClicker === RED ? false : true}
+          onClick={() => changeColor()}
+        >
+          {backgroundClicker === RED ? (
+            <Alert>Wait</Alert>
+          ) : (
+            <Alert>Click</Alert>
+          )}
+        </MainContent>
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+const MainContent = styled.div`
+  width: 60vw;
+  height: 60vh;
+  margin: auto auto;
+  display: flex;
+  background-color: ${(props) => (props.$start === false ? RED : GREEN)};
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Alert = styled.h1`
+  font-size: 3vw;
+  margin: auto auto;
+  user-select: none;
+`;
 
 export default ReactionTime;
