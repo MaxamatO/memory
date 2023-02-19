66186a74d81093257b31694bb87212b46aad46ea
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-02-19 14:27:59 +0100

    make it prettier

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Pairs from "./pairs_game/Pairs";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import "./index.css";
 import ReactionTime from "./reaction_time/ReactionTime";
 import TypingTest from "./typing_test/TypingText";
@@ -26,19 +26,27 @@ function Home() {
   return (
     <>
       {settings.game === "" ? (
-        <MenuContainer>
-          <GameOption onClick={() => setSettings({ game: "Find Pairs" })}>
-            Find Pairs
-          </GameOption>
-          <GameOption
-            onClick={() => setSettings({ game: "Reaction Time Test" })}
-          >
-            Reaction Time Test <br />
-          </GameOption>
-          <GameOption onClick={() => setSettings({ game: "Typing Test" })}>
-            Typing Speed Test <br />
-          </GameOption>
-        </MenuContainer>
+        <MainContainer>
+          <MenuContainer>
+            <GameOptionDiv onClick={() => setSettings({ game: "Find Pairs" })}>
+              <GameOption>Find Pairs</GameOption>
+            </GameOptionDiv>
+            <GameOptionDiv
+              onClick={() => setSettings({ game: "Reaction Time Test" })}
+            >
+              <GameOption>
+                Reaction Time
+                <br />
+              </GameOption>
+            </GameOptionDiv>
+            <GameOptionDiv onClick={() => setSettings({ game: "Typing Test" })}>
+              <GameOption>
+                Typing Speed
+                <br />
+              </GameOption>
+            </GameOptionDiv>
+          </MenuContainer>
+        </MainContainer>
       ) : (
         renderGame()
       )}
@@ -46,24 +54,51 @@ function Home() {
   );
 }
 
-const MenuContainer = styled.div`
+const MainContainer = styled.div`
+  height: 100vh;
   width: 100vw;
-  height: 20vh;
-  background-color: var(--board-color);
+  display: flex;
+  place-content: center;
+  margin: auto auto;
+`;
+
+const MenuContainer = styled.div`
+  border-radius: 2vw;
+  width: 70vw;
+  height: 70vh;
+  background-image: linear-gradient(to right, var(--board-color), #e0dfdf9d);
   display: flex;
   place-content: center;
   align-items: center;
   justify-content: space-evenly;
   margin: auto auto;
+  padding: 1vw;
 `;
 
-const GameOption = styled.a`
-  text-decoration: none;
-  font-size: 2.2em;
+const HeadingContainer = styled.div`
+  width: 100%;
+  height: 30%;
+  background-color: black;
+`;
+
+const GameOptionDiv = styled.div`
+  height: 50%;
+  width: 30%;
+  display: flex;
+  margin: auto auto;
+  place-content: center;
+  border-radius: 10%;
+  transition: 0.2s;
   :hover {
     cursor: pointer;
-    color: var(--bg-color);
+    color: #618dbe;
+    background-color: white;
   }
+`;
+const GameOption = styled.h1`
+  text-decoration: none;
+  font-size: 2em;
+  align-self: center;
 `;
 
 export default Home;
